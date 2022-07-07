60a9f66d134114dbc5df30824d8f40825ff4ba66
Huseyn JavaDle <huseynjavadle@gmail.com>
2022-07-07 23:55:07 +0400

    Add Models and updates
    Add seo and settings models and updates

---
+++ database/migrations/2022_07_07_194702_create_settings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->json('admin_mails_alert')->nullable();
+            $table->enum('site_status', ['online', 'offline', 'development'])->default('online');
+            $table->enum('debug_mode', ['enabled', 'disabled'])->default('disabled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+};
